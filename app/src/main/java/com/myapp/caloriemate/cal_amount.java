405ff812643930582275898dcda366d5f450a79b
Anshuman Rohella via macbook <rohella.anshuman@gmail.com>
2017-11-30 23:44:15 +0900

    added other classes

---
+++ app/src/main/java/com/myapp/caloriemate/cal_amount.java
@@ -0,0 +1,79 @@
+package com.myapp.caloriemate;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class cal_amount extends Activity {
+    Intent curr;
+    TextView maintain;
+    TextView loseless;
+    TextView losemore;
+    TextView gainless;
+    TextView gainmore;
+    float weight;
+    float height;
+    float age;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cal_amount);
+        curr = getIntent();
+
+        maintain = (TextView) findViewById(R.id.maintain_text);
+        loseless = (TextView) findViewById(R.id.lose0_5_text);
+        losemore = (TextView) findViewById(R.id.lose1_text);
+        gainless = (TextView) findViewById(R.id.gain0_5_text);
+        gainmore = (TextView) findViewById(R.id.gain1_text);
+
+        weight = curr.getFloatExtra("weight",0);
+        age = curr.getFloatExtra("age",0);
+        height =curr.getFloatExtra("height",0);
+
+
+        calc_requirement(curr.getStringExtra("activity"),curr.getStringExtra("gender"));
+
+    }
+
+
+    private void calc_requirement(String activity,String gender){
+        double cal_req= 0;
+        double BMR =0 ;
+        if(gender.compareTo("Male") == 0)
+        {
+            BMR = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age );
+        }else
+            BMR =  447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age);
+
+        switch (activity){
+            case "Sedentary- Little or no exercise.":{
+                cal_req = BMR * 1.2;
+
+                break;
+            }
+            case "Light- exercise/sports (1-3 times/week)":{
+                cal_req = BMR * 1.375;
+                break;
+            }
+            case "Moderate - exercise/sports (3-5 times/week)":{
+                cal_req = BMR * 1.55;
+                break;
+            }
+            case "High - exercise/sports (6-7 times/week)":{
+                cal_req = BMR * 1.725;
+                break;
+            }
+            case "Heavy - very hard sports/exercise (twice/day)":{
+                cal_req = BMR * 1.9;
+                break;
+            }
+        }
+        maintain.setText("To maintain your weight :"+(cal_req)+" Kcal/day");
+        loseless.setText("To lose 0.5 kgs per week : "+(cal_req-500)+" Kcal/day");
+        losemore.setText("To lose 1 Kgs per week : "+(cal_req-1000)+" Kcal/day");
+        gainless.setText("To gain 0.5 Kgs per week : "+(cal_req+500)+" Kcal/day");
+        gainmore.setText("To gain 1.0 Kgs per week : "+(cal_req+1000)+" Kcal/day");
+    }
+}
--- app/src/main/java/com/myapp/caloriemate/weeklyroutine.java
+++ app/src/main/java/com/myapp/caloriemate/weeklyroutine.java
@@ -1,13 +1,114 @@
 package com.myapp.caloriemate;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class weeklyroutine extends Activity {
+    Intent i;
+    private static float weight;
+    private static float height;
+    private static float age;
+    private static String activity;
 
+    TextView bmi_val;
+    TextView bmi_desc;
+    RadioGroup activity_group;
+    Button cal_calc;
+    Intent intent ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weeklyroutine);
+
+
+        cal_calc = (Button) findViewById(R.id.calc_calories);
+        activity_group = (RadioGroup) findViewById(R.id.radioGroup);
+        i = getIntent();
+        weight = Float.parseFloat(i.getStringExtra("weight"));
+        height = Float.parseFloat(i.getStringExtra("height"));
+        age = Float.parseFloat(i.getStringExtra("age"));
+
+        bmi_val = (TextView) findViewById(R.id.bmi_val);
+        bmi_desc = (TextView) findViewById(R.id.bmi_desc);
+        bmi_val.setText(String.valueOf(calcBMI(weight, (height / 100))));
+        cal_calc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RadioButton temp;
+                temp = (RadioButton) findViewById(activity_group.getCheckedRadioButtonId());
+                activity = String.valueOf(temp.getText());
+                caluclate_calories();
+            }
+        });
+
     }
+    private float calcBMI(float weight,float height){
+        float retval;
+        DecimalFormat df = new DecimalFormat("###.##");
+
+
+        retval = Float.parseFloat(df.format((weight / (Math.pow(height,2)))));
+        if(retval < 15.0)
+        {  bmi_desc.setText("Very severely underweight");
+            bmi_desc.setTextColor(Color.RED);
+        }
+        else if(retval > 15.0 &&  retval < 16.0)
+        {  bmi_desc.setText("Severely underweight");
+            bmi_desc.setTextColor(Color.RED);
+        }
+        else if(retval > 16.0 &&  retval < 18.5)
+        { bmi_desc.setText("Underweight");
+            bmi_desc.setTextColor(Color.YELLOW);
+        }
+        else if(retval > 18.5 &&  retval < 25.0)
+        { bmi_desc.setText("Normal");
+            bmi_desc.setTextColor(Color.GREEN);
+        }
+        else if(retval > 25.0 &&  retval < 30.0)
+        {  bmi_desc.setText("Overweight");
+            bmi_desc.setTextColor(Color.YELLOW);
+        }
+        else if(retval > 30.0 &&  retval < 35.0)
+        { bmi_desc.setText("Moderately obese");
+            bmi_desc.setTextColor(Color.parseColor("#FF4500"));
+
+        }
+        else if(retval > 35.0 &&  retval < 40.0)
+        {bmi_desc.setText("Severely obese");
+            bmi_desc.setTextColor(Color.RED);
+        }
+        else
+        { bmi_desc.setText("Morbidly obese");
+            bmi_desc.setTextColor(Color.RED);
+        }
+
+
+        return retval;
+
+
+    }
+    private void caluclate_calories(){
+
+        intent  = new Intent(this,cal_amount.class);
+        intent.putExtra("age",age);
+        intent.putExtra("weight",weight);
+        intent.putExtra("height",height);
+        intent.putExtra("activity",activity);
+        intent.putExtra("gender",i.getStringExtra("gender"));
+        startActivity(intent);
+
+
+    }
+
+
 }
+
